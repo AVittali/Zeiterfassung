6afc7c8d478d6faac2014a99a68a004a20b9d885
Andreas Vittali <vittali@gmx.de>
2023-03-18 21:09:56 +0100

    [FIX] Fehlermeldung auf Konsole behoben

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,6 @@ import {MatTabsModule} from '@angular/material/tabs';
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent, ListArbeitszeit]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
