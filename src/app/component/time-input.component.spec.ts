75659ff0eb7655af257fd45a71a0ae6343a14976
Andreas Vittali <vittali@gmx.de>
2023-04-28 18:00:49 +0200

    [FEATURE] Tests für Validate-Methode

--- src/app/component/time-input.component.spec.ts
+++ src/app/component/time-input.component.spec.ts
@@ -1,3 +1,47 @@
+import { AbstractControl } from "@angular/forms";
+import { TimeInputDirective } from "./time-input.component";
+import { ElementRef } from '@angular/core';
+
+describe('TimeInputDirective', () => {
+
+    it('Validate 800', () => {
+        // var mockElement = { value: '800' } as unknown as ElementRef<HTMLInputElement>;
+        var mockControl = { value: '800' } as unknown as AbstractControl<any, any>;
+        const result = new TimeInputDirective(null as unknown as ElementRef<HTMLInputElement>).validate(mockControl);
+        expect(result).toBe(null);
+    });
+
+    it('Validate 899', () => {
+        // var mockElement = { value: '899' } as unknown as ElementRef<HTMLInputElement>;
+        var mockControl = { value: '899' } as unknown as AbstractControl<any, any>;
+        const result = new TimeInputDirective(null as unknown as ElementRef<HTMLInputElement>).validate(mockControl);
+        expect(result).toEqual({ invalidMinuten: Object({ minuten: 99 }) });
+    });
+
+    it('Validate 2500', () => {
+        // var mockElement = { value: '800' } as unknown as ElementRef<HTMLInputElement>;
+        var mockControl = { value: '2500' } as unknown as AbstractControl<any, any>;
+        const result = new TimeInputDirective(null as unknown as ElementRef<HTMLInputElement>).validate(mockControl);
+        expect(result).toEqual({ invalidStunden: Object({ stunden: 25 }) });
+    });
+
+    it('Validate 0025', () => {
+        var mockControl = { value: '0025' } as unknown as AbstractControl<any, any>;
+        const result = new TimeInputDirective(null as unknown as ElementRef<HTMLInputElement>).validate(mockControl);
+        expect(result).toBe(null);
+    });
+
+    // it('onInputChange 8', () => {
+    //     var mockElement = null as unknown as ElementRef<HTMLInputElement>;
+    //     var mockHTMLInputElement = { value: '8' } as unknown as HTMLInputElement;
+    //     const directive = new TimeInputDirective(mockElement);
+    //     directive['onInputChange'](mockHTMLInputElement);
+    //     expect(directive['zeitValue']).toBe(800);
+    // });
+
+});
+
+
 // import { Component, DebugElement } from '@angular/core';
 // import { ComponentFixture, TestBed } from '@angular/core/testing';
 // import { By } from '@angular/platform-browser';
